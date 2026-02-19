0829ae7714382c9084bb9f7312f7c16a3401b993
Danylo Moroz <danylomoroz1989@gmail.com>
2026-02-19 23:10:39 +0100

    ingress_uncommented_in_bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-kubectl apply -f infrastructure/mysql/ns.yml
+
+kubectl create ns todoapp
 kubectl apply -f infrastructure/mysql/configMap.yml
 kubectl apply -f infrastructure/mysql/secret.yml
 kubectl apply -f infrastructure/mysql/service.yml
@@ -17,4 +18,4 @@ kubectl apply -f infrastructure/app/deployment.yml
 
 # Install Ingress Controller
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
-# kubectl apply -f infrastructure/ingress/ingress.yml
+kubectl apply -f infrastructure/ingress/ingress.yml
